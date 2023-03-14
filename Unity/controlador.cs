339f49825cae706b6b6fa69ffb024e27588df7df
Esteban Martinez <77166759+a00831789@users.noreply.github.com>
2023-03-14 16:39:41 -0600

    clase json

--- Unity/controlador.cs
+++ Unity/controlador.cs
@@ -3,6 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 
+
+// ------Clase que se obtendría del json-------
+public class Frame {
+    public Dictionary<int, Info> frame { get; set; }
+}
+
+public class Info {
+    public Dictionary<string, int[]> posiciones { get; set; }
+    public Dictionary<int[], int> calles { get; set; }
+    public Dictionary<int[], int> paquetes { get; set; }
+}
+
+
+
+
+// ---- Request de servidor (aun falta de implemntar)-------
 public class web : MonoBehaviour
 {
     [ContextMenu("Leer simple")]
